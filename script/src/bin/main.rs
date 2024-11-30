1093089235dbff1a4b9c12ba20a3a75537b40a8d
armanthepythonguy <armanityours@gmail.com>
2024-11-30 22:21:21 +0530

    orderbook-optimization

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -12,7 +12,7 @@
 
 use alloy_sol_types::SolType;
 use clap::Parser;
-use fibonacci_lib::{Order, State, OrderType, Trade};
+use fibonacci_lib::{match_order, Order, OrderType, State, Trade};
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
 use serde::{Serialize, Deserialize};
 
@@ -52,11 +52,22 @@ fn main() {
     // Setup the prover client.
     let client = ProverClient::new();
 
+    //Generating the inputs and outputs
+    let start_state = State{pending_ask_orders:vec![], pending_bid_orders: vec![], trades: vec![]};
+    let mut transactions: Vec<Order> = vec![];
+    transactions.push(Order{id:"123".to_string(), order_type:OrderType::Bid, price: 1.05, quantity: 1000});
+    transactions.push(Order{id:"123".to_string(), order_type:OrderType::Ask, price: 1.05, quantity: 1000});
+
+    let mut last_state = start_state.clone();
+    for tx in transactions.iter(){
+        last_state = match_order(last_state, tx.clone());
+    }
+    println!("{:?}", last_state);
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
-    stdin.write(&State{pending_ask_orders:vec![], pending_bid_orders: vec![], trades: vec![]});
-    stdin.write(&vec![Order{id:"123".to_string(), order_type:OrderType::Bid, price: 1.05, quantity: 1000}, Order{id:"123".to_string(), order_type:OrderType::Ask, price: 1.05, quantity: 1000}]);
-    stdin.write(&State{pending_ask_orders:vec![], pending_bid_orders: vec![], trades: vec![Trade{id:"123123".to_string(), price: 1.05, quantity: 1000, ask_order:Order{id:"123".to_string(), order_type:OrderType::Ask, price: 1.05, quantity: 1000}, bid_order:Order{id:"123".to_string(), order_type:OrderType::Bid, price: 1.05, quantity: 1000}}]});
+    stdin.write(&start_state);
+    stdin.write(&transactions);
+    stdin.write(&last_state);
     // println!("n: {}", args.n);
 
     if args.execute {
